2a3f599148baa0bdc712b056953b40804464d3e1
nancyanumba <anumbanancy@gmail.com>
2023-04-21 11:11:48 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - assigns a random number to variable n, and prints whether the number is positive or negative.
  *
- * Return: Zero 0 on success
+ * Return: Zero on success
  */
 int main(void)
 {
