02f4472a3fa25f2927d27c7cca7145708264cbc2
nancyanumba <anumbanancy@gmail.com>
2023-04-21 11:30:23 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: program that prints the alphabet in lowercase, followed by a new line.
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+			continue;
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
